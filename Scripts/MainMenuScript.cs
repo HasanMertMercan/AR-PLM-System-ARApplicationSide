c37ff144bf3ebcd09a5660246fccb8b7e48192bd
Hasan Mert Mercan <HasanMertMercan@users.noreply.github.com>
2018-08-12 20:03:00 +0200

    Add files via upload
    20180812-evening

---
+++ Scripts/MainMenuScript.cs
@@ -0,0 +1,43 @@
+﻿using Assets.Entity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour {
+
+    private List<MachineEntity> optimisedMachineList = new List<MachineEntity>();
+    public Text txtErrorMessage;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void FactoryButtonOnClick()
+    {
+        SceneManager.LoadScene("ReadQrCode");
+    }
+
+    public void MachineButtonOnClick()
+    {
+        SceneManager.LoadScene("ReadQRCode");
+    }
+
+    public void ListButtonOnClick()
+    {
+        if(optimisedMachineList.Count == 0)
+        {
+            txtErrorMessage.text = "There is No Root to Display";
+        }
+        else
+        {
+            //Display the root for machines
+        }
+    }
+}
